f7c6e83a393101bad15b43e8752fe7a480e23013
Jahnvi Srivastava <jahnvisrivastava0104@gmail.com>
2026-09-12 20:31:43 +0530

    Add Firebase Google authentication setup

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,21 +1,14 @@
 import admin from "firebase-admin";
 import dotenv from "dotenv";
+import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
 
 dotenv.config();
 
-const { FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } = process.env;
+export const isFirebaseConfigured = true;
 
-export const isFirebaseConfigured = Boolean(
-  FIREBASE_PROJECT_ID && FIREBASE_CLIENT_EMAIL && FIREBASE_PRIVATE_KEY
-);
-
-if (isFirebaseConfigured && !admin.apps.length) {
+if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: FIREBASE_PROJECT_ID,
-      clientEmail: FIREBASE_CLIENT_EMAIL,
-      privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
